d635793e9fcb8434aa1235d409cab58d62d3bf11
juan-dio <juanaxl432@gmail.com>
2024-08-04 14:09:47 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// NAVBAR
+window.onscroll = () => {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add('header-fixed');
+  } else {
+    header.classList.remove('header-fixed');
+  }
+}
+
+const navBtn = document.querySelector('#nav-btn');
+
+navBtn.addEventListener('click', () => {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('active');
+  navBtn.classList.toggle('active');
+});
+
+// DASHBOARD
+const sentence = document.querySelector('#sentence');
+
+const text = ["Informatics Engineering", "Software Engineer"];
+let i = 0;
+let j = 0;
+
+setInterval(() => {
+  if (j < text[i].length) {
+    sentence.innerHTML += text[i].charAt(j);
+    j++;
+    if (j == text[i].length) {
+      setTimeout(() => {
+        sentence.innerHTML = '';
+        setTimeout(() => {
+          j = 0;
+          i++;
+          if (i == text.length) {
+            i = 0;
+          }
+        }, 1500)
+      }, 1500);
+    }
+  }
+}, 200);
+
+
+// SKILLS
+const logo = document.querySelector('.skills-logo').cloneNode(true);
+document.querySelector('.skills-logo-container').appendChild(logo);
